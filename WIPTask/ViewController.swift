4892e2b8709f27eb419d961ddf72778c300b6085
Andreea Ioana Muresan <andreeaioanamuresan95@gmail.com>
2021-01-17 19:32:54 +0200

    Initial Commit

---
+++ WIPTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WIPTask
+//
+//  Created by Andreea Ioana Muresan on 17/01/2021.
+//  Copyright © 2021 wipTask. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
